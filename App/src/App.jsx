5b1fbccb12ee36c799881dabd2ed6b73cb9de8dc
LeofenBon <46328889+LeofenBon@users.noreply.github.com>
2023-04-04 02:28:06 +0400

    added theme to project - customized color pallete - added typography - changed title of page - changed favicon

--- App/src/App.jsx
+++ App/src/App.jsx
@@ -2,7 +2,24 @@ import logo from './logo.svg';
 import styles from './App.module.css';
 
 function App() {
-  return <h1 class="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <main class="bg-white">
+        <h1 class="text-3xl font-bold font-poppins text-theme-smoky">
+          Hello world!
+        </h1>
+        <h1 class="text-3xl font-martel text-theme-mantis">
+          This is sub-heading
+        </h1>
+        <h1 class="font-hind">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti
+          nulla, alias in, tempora doloribus consequuntur consectetur ut facilis
+          voluptates repellendus inventore officia recusandae vel dolore natus
+          harum atque aspernatur reprehenderit?
+        </h1>
+      </main>
+    </>
+  );
 }
 
 export default App;
--- App/tailwind.config.js
+++ App/tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        theme: {
+          smoky: "#131200",
+          mantis: "#78BC61",
+          purple: "#6E2594",
+          salmon: "#E9806E",
+        },
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+        hind: ["Hind Guntur", "sans-serif"],
+        martel: ["Martel", "serif"],
+      },
+    },
   },
   plugins: [],
 };
